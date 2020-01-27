0c1acbe323fecee0d03a97fa4cecec9556da45dd
vps ftp user account <justin@php-m.mirohost.net>
2020-01-27 12:07:34 +0200

    setup done

---
+++ public/.env.local.php
@@ -0,0 +1,9 @@
+
+<?php
+return [
+    'dbname' => 'serviceapitest',
+    'username' => 'u_serviceapihv',
+    'password' => '3k0rUlLayykq',
+    'hostname' => 'localhost',
+    'sqlchar' => 'utf8',
+];
